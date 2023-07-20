80bc06c08aefc871a77c0ebb4761bec1c5d1424a
Mauricio220 <mauchavezmeji34@gmail.com>
2023-07-20 15:46:15 -0500

    inicio de secion 12-07-2021

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inicio de cesion 
+    </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body style="background-image: url(img/2.jpg); background-repeat: no-repeat; background-size: cover;  ">
+
+     
+    <div x class="container">
+        <div class="row">
+            <div class="col-md-5 mx-auto my-5"> 
+
+                <div class="card " >
+                    <div class="card-body text-center"  style=" background-color: rgba(240, 250, 255,0.5);">
+                      <div class="from text-alaing p-3" >
+                         <h1>Iniciar cesion</h1>
+                         <div>
+                             <img style="border-radius: 100%;" src="img/unnamed.jpg" width="200px"  height="200px" >
+                         </div>
+                          <div class="p-3">
+                            <input type="email" class="form-control">
+                          </div>
+                          <div class="p-3">
+
+                            <input type="password"class="form-control">
+                          </div>
+                          <a href="#">Iniciar cesion</a>
+                          
+                      </div>
+
+
+
+                    </div>
+                  </div>
+            </div>
+        </div>
+    </div>
+
+
+ 
+    
+</body>
+</html>
